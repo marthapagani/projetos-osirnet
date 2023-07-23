723d0001c35185d9fe5322289154c29d3765492d
Martha Pagani <22531166+marthapagani@users.noreply.github.com>
2023-07-22 21:03:38 -0300

    Separados projetos do trabalho dos pessoais

---
+++ info-complementar.py
@@ -0,0 +1,32 @@
+def router ():
+    equipamento = input('Marca da ONU: ')
+    serial = input('Serial do equipamento: ')
+    slot = int(input('Slot: '))
+    porta = int(input('Porta: '))
+    id = int(input('ID: '))
+    ssid = input('SSID: ')
+    senha = input('Senha: ')
+
+    print('Informação complementar: ')
+    print(f'{equipamento.capitalize()} Router || SN: {serial.upper()} || Slot: {slot} Porta: {porta} ID: {id} || SSID: {ssid} - Senha: {senha}')
+
+def bridge():
+    equipamento = input('Marca da ONU em bridge: ')
+    serial = input('Serial do equipamento: ')
+    router = input('Marca/modelo do roteador: ')
+    slot = int(input('Slot: '))
+    porta = int(input('Porta: '))
+    id = int(input('ID: '))
+    ssid = input('SSID: ')
+    senha = input('Senha: ')
+
+    print('Informação complementar: ')
+    print(f'{equipamento.capitalize()} Bridge || SN: {serial.upper()} || Autentica no {router.capitalize()} || Slot: {slot} Porta: {porta} ID: {id} || SSID: {ssid} - Senha: {senha}')
+
+
+tipo = int(input('Digite:\n[1] Router\n[2] Bridge\n\n'))
+
+if tipo == 1:
+    router()
+else:
+    bridge()
