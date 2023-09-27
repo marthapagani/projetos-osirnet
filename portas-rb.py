ed3bc73e71b7f00e20e02ec8a35e8c46170db778
Martha Pagani <22531166+marthapagani@users.noreply.github.com>
2023-09-26 23:31:13 -0300

    Inserido laço for para efetuar mais aberturas

--- portas-rb.py
+++ portas-rb.py
@@ -1,28 +1,32 @@
-def redir():
-    desc = input('PPPOE + Nome do cliente: ')
-    porta_interna = int(input('Porta interna: '))
-    ipprivado = input('IP público privado (100.65): ')
-    porta_externa = int(input('Porta externa: '))
-    ippublico = input('IP público de acesso externo: ')
+def redir(n):
+    for c in range(0, n, 1):
+        desc = input('PPPOE + Nome do cliente: ')
+        porta_interna = int(input('Porta interna: '))
+        ipprivado = input('IP público privado (100.65): ')
+        porta_externa = int(input('Porta externa: '))
+        ippublico = input('IP público de acesso externo: ')
 
-    print(f'\n/ip firewall nat add action=dst-nat chain=dstnat comment="{desc}" dst-address={ippublico} dst-port={porta_externa} protocol=tcp to-addresses={ipprivado} to-ports={porta_interna}')
-    print(f'/ip firewall nat add action=dst-nat chain=dstnat comment="{desc}" dst-address={ippublico} dst-port={porta_externa} protocol=udp to-addresses={ipprivado} to-ports={porta_interna}')
+        print(f'\n/ip firewall nat add action=dst-nat chain=dstnat comment="{desc}" dst-address={ippublico} dst-port={porta_externa} protocol=tcp to-addresses={ipprivado} to-ports={porta_interna}')
+        print(f'\n/ip firewall nat add action=dst-nat chain=dstnat comment="{desc}" dst-address={ippublico} dst-port={porta_externa} protocol=udp to-addresses={ipprivado} to-ports={porta_interna}')
 
-def abertura():
-    porta_interna = int(input('Porta interna: '))
-    porta_externa = int(input('Porta externa: '))
-    ipprivado = input('IP do equipamento: ')
+def abertura(n):
+    for c in range(0, n, 1):
+        desc = input('Descrição da porta: ')
+        porta_interna = int(input('Porta interna: '))
+        porta_externa = int(input('Porta externa: '))
+        ipprivado = input('IP do equipamento: ')
 
-    print(f'\n/ip firewall nat add action=dst-nat chain=dstnat dst-port={porta_interna} protocol=tcp to-addresses={ipprivado} to-ports={porta_externa}')
-    print(f'/ip firewall nat add action=dst-nat chain=dstnat dst-port={porta_interna} protocol=udp to-addresses={ipprivado} to-ports={porta_externa}')
+        print(f'\n/ip firewall nat add action=dst-nat chain=dstnat comment="{desc}" dst-port={porta_interna} protocol=tcp to-addresses={ipprivado} to-ports={porta_externa}')
+        print(f'/ip firewall nat add action=dst-nat chain=dstnat comment="{desc}" dst-port={porta_interna} protocol=udp to-addresses={ipprivado} to-ports={porta_externa}')
 
 # INÍCIO
 
-tipo = int(input('Digite:\n[1] Redirecionamento de portas\n[2] Abertura de portas\n\n'))
+tipo = int(input('Opção:\n[1] Redirecionamento de portas\n[2] Abertura de portas\n\n'))
+n = int(input('Quantas aberturas ou redirecionamentos? ')) 
 
 if tipo == 1:
-    redir()
+    redir(n)
 else:
-    abertura()
+    abertura(n)
 
 #TODO tratamento de erros
