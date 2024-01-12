62bf20490364c1cb8bd2ee20430afc0b20663681
Martha Pagani <22531166+marthapagani@users.noreply.github.com>
2024-01-11 22:06:01 -0300

    Atualização do script de cálculo de horas

--- Python/data.py
+++ Python/data.py
@@ -3,28 +3,28 @@ from datetime import datetime
 def diferenca_horas(hora1, hora2, data1, data2):
     try:
         # Combinando as strings de data e hora para formar objetos datetime
-        dt1 = datetime.strptime(f'{data1} {hora1}', '%d-%m-%Y %H:%M')
-        dt2 = datetime.strptime(f'{data2} {hora2}', '%d-%m-%Y %H:%M')
+        dt1 = datetime.strptime(f'{data1} {hora1}', '%d%m%Y %H:%M')
+        dt2 = datetime.strptime(f'{data2} {hora2}', '%d%m%Y %H:%M')
 
         # Calculando a diferença
         diferenca = dt2 - dt1
 
-        # Exibindo a diferença em dias, horas e minutos
-        dias = diferenca.days * 24
-        horas, remainder = divmod(diferenca.seconds, 3600)
-        horas = horas + dias
-        minutos = remainder // 60 
+        # Exibindo a diferença em horas e minutos
+        dias = diferenca.days
+        horas, resto = divmod(diferenca.seconds, 3600)
+        horas =+ horas + (dias * 24)
+        minutos = resto // 60
 
-        print(f'Diferença: {horas}:{minutos}.')
+        print(f'{horas}:{minutos} de diferença')
 
     except ValueError:
-        print("Formato inválido. Certifique-se de usar o formato 'DIA-MES-ANO e HORA:MINUTO'.")
+        print("Formato inválido. Certifique-se de usar o formato 'DIA/MÊS/ANO HH:mm'.")
 
 if __name__ == "__main__":
-    data_inicio = input("Digite a data de início (formato DIA-MES-ANO): ")
-    hora_inicio = input("Digite a hora de início (formato HORA:MINUTO): ")
+    data_inicio = input("Data de início (formato DIA/MÊS/ANO): ")
+    hora_inicio = input("Hora de início (formato HH:mm): ")
     
-    data_fim = input("Digite a data de término (formato DIA-MES-ANO): ")
-    hora_fim = input("Digite a hora de término (formato HORA:MINUTO): ")
+    data_fim = input("Data de término (formato DIA/MÊS/ANO): ")
+    hora_fim = input("Hora de término (formato HH:mm): ")
 
     diferenca_horas(hora_inicio, hora_fim, data_inicio, data_fim)
